75830fca85406bd29246bb82bb382e57198e0ed5
David <143638937+Cheessse@users.noreply.github.com>
2024-03-30 12:56:33 +0000

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import Description from './components/Description/Description'
 import Options from './components/Options/Options'
@@ -6,11 +6,30 @@ import Feedback from './components/Feedback/Feedback'
 import Notification from './components/Notification/Notification'
 
 function App() {
-  const [feedback, setFeedback] = useState({
-	good: 0,
-	neutral: 0,
-	bad: 0
+  const [feedback, setFeedback] = useState(() => {
+    const savedFeedback = localStorage.getItem('feedback-quantity')
+    if (savedFeedback !== null) {
+      return JSON.parse(savedFeedback)
+    } else {
+      return {
+      good: 0,
+      neutral: 0,
+      bad: 0
+      }
+    }
   })
+
+  useEffect(() => {
+    window.localStorage.setItem('feedback-quantity', JSON.stringify(feedback))
+  }, [feedback])
+
+  const resetFeedback = () => {
+    setFeedback({
+      good: 0,
+      neutral: 0,
+      bad: 0
+    })
+  }
   
 const updateFeedback = (feedbackType) => {
   setFeedback({
@@ -21,14 +40,23 @@ const updateFeedback = (feedbackType) => {
   
   const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
 
+  const positiveRatio = Math.round((feedback.good / totalFeedback) * 100);
+
+
 
 
 
   return (
     <>
       <Description />
-      <Options updateFeedback={updateFeedback} />
-     {totalFeedback > 0 ? ( <Feedback good={feedback.good} neutral={feedback.neutral} bad={feedback.bad} /> ) : (<Notification />)}
+      <Options  updateFeedback={updateFeedback}
+                resetFeedback={resetFeedback}
+                totalFeedback={totalFeedback} />
+      {totalFeedback > 0 ? (<Feedback good={feedback.good}
+                                      neutral={feedback.neutral}
+                                      bad={feedback.bad}
+                                      totalFeedback={totalFeedback}
+                                      positiveRatio={positiveRatio} />) : (<Notification />)}
     </>
   )
 }
--- src/components/Options/Options.jsx
+++ src/components/Options/Options.jsx
@@ -1,16 +1,16 @@
 import css from "./Options.module.css";
 
-const Options = ({updateFeedback}) => {
+const Options = ({updateFeedback, resetFeedback, totalFeedback}) => {
   return (
     <div className={css.wrapper}>
       <button onClick={() => updateFeedback('good')}>Good</button>
       <button onClick={() => updateFeedback('neutral')}>Neutral</button>
       <button onClick={() => updateFeedback('bad')}>Bad</button>
-      {/* {totalFeedback > 0 && (
+      {totalFeedback > 0 && (
         <button onClick={resetFeedback} className={css.btn}>
           Reset
         </button>
-      )} */}
+      )}
     </div>
   );
 };
